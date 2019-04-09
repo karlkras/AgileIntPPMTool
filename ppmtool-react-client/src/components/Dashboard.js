243a47ad95f66a6c29ad48292181e5fd5344946b
karlkras <karlkras@yahoo.com>
2019-04-09 15:56:38 -0700

    completed branch19, yay!

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,34 +1,30 @@
 import React, {Component, Fragment} from "react";
-import PropTypes from "prop-types";
 import ProjectItem from "./Project/ProjectItem";
 import "bootstrap/dist/css/bootstrap.min.css";
 import CreateProjectButton from "./Project/CreateProjectButton";
 
 class Dashboard extends Component {
-    static propTypes = {
-        prop: PropTypes
-    };
 
-    render() {
-        return (
-            <Fragment>
-                <div className="projects">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-md-12">
-                                <h1 className="display-4 text-center">Projects</h1>
-                                <br/>
-                                <CreateProjectButton/>
-                                <br/>
-                                <hr/>
-                                <ProjectItem/>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </Fragment>
-        );
-    }
+  render() {
+    return (
+      <Fragment>
+        <div className="projects">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <h1 className="display-4 text-center">Projects</h1>
+                <br/>
+                <CreateProjectButton/>
+                <br/>
+                <hr/>
+                <ProjectItem/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default Dashboard;
