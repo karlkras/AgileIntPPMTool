9158467e3eb07e5f07901c7ade70e5511859ed3c
karlkras <karlkras@yahoo.com>
2019-04-09 14:42:48 -0700

    complete branch18

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,20 +1,24 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import AddProject from './components/Project/AddProject';
+import {Provider} from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Header/>
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/addProject" component={AddProject} />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header/>
+            <Route exact path="/dashboard" component={Dashboard}/>
+            <Route exact path="/addProject" component={AddProject}/>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
